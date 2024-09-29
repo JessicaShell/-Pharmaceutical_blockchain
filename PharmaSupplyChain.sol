42f3ca977c83774a304d9ef3e6bdf42b9de14890
JessicaShell <137784003+JessicaShell@users.noreply.github.com>
2024-09-29 22:18:09 +0530

    Update PharmaSupplyChain.sol

--- PharmaSupplyChain.sol
+++ PharmaSupplyChain.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract PharmaSupplyChain {
     
-    enum BatchStatus { Manufactured, InTransit, Delivered }
+    enum BatchStatus { Manufactured, QualityCheck, InTransit, Delivered }
     
     struct DrugBatch {
         string batchNumber;
@@ -15,19 +15,12 @@ contract PharmaSupplyChain {
     }
 
     mapping(string => DrugBatch) public batches;
-
-    mapping(address => string) public ownerNames;
     
-    event BatchCreated(string batchNumber, string manufacturer, address owner, string ownerName);
+    event BatchCreated(string batchNumber, string manufacturer, address owner);
     
-    event BatchTransferred(string batchNumber, string oldOwnerName, address oldOwner, address newOwner);
+    event BatchTransferred(string batchNumber, address oldOwner, address newOwner);
     
-    function createDrugBatch(
-        string memory _batchNumber, 
-        string memory _manufacturer, 
-        string memory _expirationDate, 
-        string memory _ownerName
-    ) public {
+    function createDrugBatch(string memory _batchNumber, string memory _manufacturer, string memory _expirationDate) public {
         require(batches[_batchNumber].createdAt == 0, "Batch already exists");
 
         batches[_batchNumber] = DrugBatch({
@@ -39,42 +32,40 @@ contract PharmaSupplyChain {
             createdAt: block.timestamp
         });
 
-        ownerNames[msg.sender] = _ownerName;
-
-        emit BatchCreated(_batchNumber, _manufacturer, msg.sender, _ownerName);
+        emit BatchCreated(_batchNumber, _manufacturer, msg.sender);
     }
     
-    function transferBatch(string memory _batchNumber, address _newOwner, string memory _newOwnerName) public {
+    function transferBatch(string memory _batchNumber, address _newOwner) public {
         
         require(batches[_batchNumber].createdAt != 0, "Batch does not exist");
 
         require(batches[_batchNumber].owner == msg.sender, "Only the owner can transfer the batch");
 
-        string memory oldOwnerName = ownerNames[msg.sender];
-
         address oldOwner = batches[_batchNumber].owner;
         batches[_batchNumber].owner = _newOwner;
 
-        ownerNames[_newOwner] = _newOwnerName;
-
-        emit BatchTransferred(_batchNumber, oldOwnerName, oldOwner, _newOwner);
+        emit BatchTransferred(_batchNumber, oldOwner, _newOwner);
     }
     
-    function getBatch(string memory _batchNumber) public view returns (
-        string memory, string memory, string memory, address, BatchStatus, uint
-    ) {
+    function getBatch(string memory _batchNumber) public view returns (string memory, string memory, string memory, address, BatchStatus, uint) {
         DrugBatch memory batch = batches[_batchNumber];
         require(batch.createdAt != 0, "Batch does not exist");
 
         return (batch.batchNumber, batch.manufacturer, batch.expirationDate, batch.owner, batch.status, batch.createdAt);
     }
     
-    function updateBatchStatus(string memory _batchNumber, BatchStatus _status) public {
-      
+    function updateBatchStatus(string memory _batchNumber, uint _status) public {
+       
         require(batches[_batchNumber].createdAt != 0, "Batch does not exist");
 
         require(batches[_batchNumber].owner == msg.sender, "Only the owner can update the batch status");
 
-        batches[_batchNumber].status = _status;
+        if (_status == 2) {
+            batches[_batchNumber].status = BatchStatus.InTransit;
+        } else if (_status == 3) {
+            batches[_batchNumber].status = BatchStatus.Delivered;
+        } else {
+            revert("Invalid status. Use 2 for InTransit and 3 for Delivered.");
+        }
     }
 }
